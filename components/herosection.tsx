cf24611e17d2d8a0ddb3a4d8c9e4606bf81230b4
kirti <kirti25032007@gmail.com>
2025-10-24 23:22:46 +0530

    welcome page

--- components/herosection.tsx
+++ components/herosection.tsx
@@ -1,7 +1,9 @@
 export default function HeroSection() {
   return (
     <div>
-      <h1 className="text-5xl font-bold">Welcome to Baatcheet</h1>
+      <h1 className="text-5xl font-bold">
+        Welcome to baatcheet your digital self
+      </h1>
     </div>
   );
 }
