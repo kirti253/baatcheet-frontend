23168182ef4b6dda935b9c7b3a418ee884b54d17
kirti <kirti25032007@gmail.com>
2025-10-30 22:18:45 +0530

    added navbar

--- components/header.tsx
+++ components/header.tsx
@@ -4,6 +4,19 @@ export default function Header() {
       <div>
         <h1 className="text-2xl font-bold">Baatcheet</h1>
       </div>
+      <div>
+        <nav className="space-x-4">
+          <a href="http://" className="hover:underline">
+            Home
+          </a>
+          <a href="http://" className="hover:underline">
+            About
+          </a>
+          <a href="http://" className="hover:underline">
+            Contact
+          </a>
+        </nav>
+      </div>
 
       <div className="flex space-x-2 text-xl">
         <a href="http://">Sign In</a>
